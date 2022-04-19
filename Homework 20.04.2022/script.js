3c539990a810e4d685aa41cf1d82335558eedb9f
Encors <cheperish@mail.ru>
2022-04-20 02:54:31 +0300

    Create script.js

---
+++ Homework 20.04.2022/script.js
@@ -0,0 +1,25 @@
+// 4 ЗАДАНИЕ
+
+let age_2 = 18
+let age_3 = 60
+let data = prompt("Сообщите свой возраст, пожалуйста") 
+const checkAge = function(age) {
+    let count = Number(checkAge)
+    
+    if (typeof(count) == "number") {
+        if (age < age_2) {
+            return ("You don’t have access cause your age is " + age + ". It’s less then 18")
+        } else if (age >= age_2 && age < age_3) {
+            return ("You are welcome!")
+        } else if (age > age_3) {
+            return ("Keep calm and look Culture channel")
+        } else {
+            return ("Technical work")
+        } 
+    } else {
+        return ("Error")
+        }
+
+}
+
+alert(checkAge(data))
