6590a12005be4f523efcca2b7c05d0fc797adcf8
Encors <cheperish@mail.ru>
2022-04-20 02:54:21 +0300

    Create HM2star.js

---
+++ Homework 20.04.2022/HM2star.js
@@ -0,0 +1,68 @@
+// 1 ЗАДАНИЕ
+
+let age_2 = 18
+let age_3 = 60
+const checkAge = function(age) {
+    if (age < age_2) {
+        return ("You don’t have access cause your age is " + age + ". It’s less then 18")
+    } else if (age >= age_2 && age < age_3) {
+        return ("You are welcome!")
+    } else if (age > age_3) {
+        return ("Keep calm and look Culture channel")
+    } else {
+        return ("Technical work")
+    }
+}
+
+console.log(checkAge(17))
+console.log(checkAge(18))
+console.log(checkAge(61))
+
+
+// 2 ЗАДАНИЕ
+
+let age_2 = 18
+let age_3 = 60
+
+const checkAge = function(age) {
+    if (typeof(age) == "number") {
+        if (age < age_2) {
+            return ("You don’t have access cause your age is " + age + ". It’s less then 18")
+        } else if (age >= age_2 && age < age_3) {
+            return ("You are welcome!")
+        } else if (age > age_3) {
+            return ("Keep calm and look Culture channel")
+        } else {
+            return ("Technical work")
+        }
+    } 
+    else {
+        return ("Error")
+    }
+}
+
+console.log(checkAge("авыа"))
+
+// 3 ЗАДАНИЕ
+
+let age_2 = 18
+let age_3 = 60
+const checkAge = function(age) {
+    let count = Number(checkAge)
+
+    if (typeof(count) == "number") {
+        if (age < age_2) {
+            return ("You don’t have access cause your age is " + age + ". It’s less then 18")
+        } else if (age >= age_2 && age < age_3) {
+            return ("You are welcome!")
+        } else if (age > age_3) {
+            return ("Keep calm and look Culture channel")
+        } else {
+            return ("Technical work")
+        } 
+    } else {
+        return ("Error")
+    }
+}
+
+console.log(checkAge("2"))
