ed60139f78b21ac84fd35bbfe59b7834379d5af8
Encors <cheperish@mail.ru>
2022-04-20 18:06:37 +0300

    Update HM2star.js

--- Homework 20.04.2022/HM2star.js
+++ Homework 20.04.2022/HM2star.js
@@ -48,9 +48,9 @@ console.log(checkAge("авыа"))
 let age_2 = 18
 let age_3 = 60
 const checkAge = function(age) {
-    let count = Number(checkAge)
+    let count = Number(age)
 
-    if (typeof(count) == "number") {
+    if (count) {
         if (age < age_2) {
             return ("You don’t have access cause your age is " + age + ". It’s less then 18")
         } else if (age >= age_2 && age < age_3) {
